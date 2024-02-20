e289674f44972fce4dabb1a34c66c86b5f898bf6
Aymene <belmegaming@gmail.com>
2024-02-20 13:05:30 +0100

    Update get_cached.py

--- utils/get_cached.py
+++ utils/get_cached.py
@@ -5,10 +5,10 @@ def search_cache(query):
     print(query)
     print("Searching for cached movies on remote server")
     if query['type'] == "movie":
-        url = os.environ.get('CACHE_URL') + /getResult/movie/"
+        url = os.environ.get('CACHE_URL') + "/getResult/movie/"
         response = requests.get(url, json=query)
         return response.json()
     if query['type'] == "series":
-        url = os.environ.get('CACHE_URL') + getResult/series/"
+        url = os.environ.get('CACHE_URL') + "/getResult/series/"
         response = requests.get(url, json=query)
         return response.json()
