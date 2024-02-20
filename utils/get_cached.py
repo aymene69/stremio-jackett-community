c3660e9a6731aa28edae4689a037c5255289b915
Aymene <belmegaming@gmail.com>
2024-02-20 11:44:24 +0100

    Update get_cached.py

--- utils/get_cached.py
+++ utils/get_cached.py
@@ -1,14 +1,14 @@
 import requests
-
+import os
 
 def search_cache(query):
     print(query)
     print("Searching for cached movies on remote server")
     if query['type'] == "movie":
-        url = "https://stremio-jackett-cacher.elfhosted.com/getResult/movie/"
+        url = os.environ.get('CACHE_URL') + /getResult/movie/"
         response = requests.get(url, json=query)
         return response.json()
     if query['type'] == "series":
-        url = "https://stremio-jackett-cacher.elfhosted.com/getResult/series/"
+        url = os.environ.get('CACHE_URL') + getResult/series/"
         response = requests.get(url, json=query)
         return response.json()
